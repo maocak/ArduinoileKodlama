08e065c68f9a1904b249aab5f76ef52066c7321d
Mehmet Akif Ocak <35992580+maocak@users.noreply.github.com>
2021-11-30 11:58:51 +0300

    Update 3.1.1_gece_lambasi.ino

--- 3.1.1_gece_lambasi/3.1.1_gece_lambasi.ino
+++ 3.1.1_gece_lambasi/3.1.1_gece_lambasi.ino
@@ -8,7 +8,7 @@ Serial.begin(9600); //seri veri girişi başlatılır.
 
 void loop() {
 ldrdeger = analogRead(ldrpin); //LDR'de okunan değer ldrdeger isimli değişkene atanır. 
-if(ldrdeger<500) digitalWrite(7, HIGH); //eğer okunan analog değer 840 dan büyükse LED yanar. 
+if(ldrdeger>500) digitalWrite(7, HIGH); //eğer okunan analog değer 840 dan büyükse LED yanar. 
 else digitalWrite(7, LOW); //değilse LED sönüktür. 
 Serial.println(ldrdeger); //LDR'ye düşen ışık miktarı değişdikçe okunan analog değer seri port ekranına yazdırılır.
 }
