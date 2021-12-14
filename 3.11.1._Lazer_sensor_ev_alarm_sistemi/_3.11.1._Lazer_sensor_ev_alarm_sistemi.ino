7f666a0cdd1c0d10a79c2786001b76b76212b008
Mehmet Akif Ocak <35992580+maocak@users.noreply.github.com>
2021-12-14 14:13:30 +0300

    Update _3.11.1._Lazer_sensor_ev_alarm_sistemi.ino

--- 3.11.1._Lazer_sensor_ev_alarm_sistemi/_3.11.1._Lazer_sensor_ev_alarm_sistemi.ino
+++ 3.11.1._Lazer_sensor_ev_alarm_sistemi/_3.11.1._Lazer_sensor_ev_alarm_sistemi.ino
@@ -16,14 +16,20 @@ void loop() {
 digitalWrite(lasersensorpin, HIGH); //Lazer sensör çalıştırılır ve hazır hale getirilir. 
 deger =analogRead(A0); //LDR pininden analog değer okunur. 
 Serial.println(deger);  //Okunan analog değer seri port ekranına yazdırılır. 
-while(deger <800)  //Eğer analog değer eşik değer olan 800 altına düşerse...
+if (deger <800)  //Eğer analog değer eşik değer olan 800 altına düşerse...
 {
 tone(buzzerpin, 440);  //Buzzer ses verir. 
 delay(50); 
-noTone(buzzerpin); //delay ve noTone, buzzerin kesikli şekilde(alarm) ses vermesini sağlar. 
+noTone(buzzerpin);
 digitalWrite(ledpin, HIGH); //LED yanar. 
 delay(100); // Bu delay LED'in devamlı olarak değil, blink olarak yanmasını sağlar. 
 digitalWrite(ledpin,LOW);
 }
+else
+{
+noTone(buzzerpin); //delay ve noTone, buzzerin kesikli şekilde(alarm) ses vermesini sağlar. 
+digitalWrite(ledpin,LOW);
+}
 delay(100); //Her 0.1 saniyede sensörden okuma yapılır. 
 }
+
