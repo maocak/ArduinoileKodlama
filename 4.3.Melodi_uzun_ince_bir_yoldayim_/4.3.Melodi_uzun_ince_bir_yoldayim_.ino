4fac109d3c8272899de2ee78c4c040ec85be9460
Mehmet Akif Ocak <35992580+maocak@users.noreply.github.com>
2021-12-14 16:41:13 +0300

    Update 4.3.Melodi_uzun_ince_bir_yoldayim_.ino

--- 4.3.Melodi_uzun_ince_bir_yoldayim_/4.3.Melodi_uzun_ince_bir_yoldayim_.ino
+++ 4.3.Melodi_uzun_ince_bir_yoldayim_/4.3.Melodi_uzun_ince_bir_yoldayim_.ino
@@ -7,13 +7,13 @@
 #define  Si     3951
 
 int buzzerpin = 3; //buzzer pini tanımlanır
-int notasayisi1 =10; //1. dizi için kullanılacak nota sayısı tanımlanır.
-int notasayisi2 =12; //2. dizi için kullanılacak nota sayısı tanımlanır.  
+int notasayisi1 =11; //1. dizi için kullanılacak nota sayısı tanımlanır.
+int notasayisi2 =13; //2. dizi için kullanılacak nota sayısı tanımlanır.  
 int notasayisi3 = 22; //3.dizi için kullanılacak nota sayısı tanımlanır. 
 int notasayisi4 = 20; //4.dizi için kullanılacak nota sayısı tanımlanır. 
   
-int dizi1[] = {Fa,Sol,Sol,Fa, Mi, Re, Fa, Mi, Re, Re}; //1.dizideki notalar
-int dizi2[]= {La, Sol, La, Fa, Sol, Fa, Mi, Re, Fa, Mi, Re, Re}; //2.dizideki notalar
+int dizi1[] = {Fa,Sol,Sol,Fa, Mi, Re, Fa, Mi, Re, Re,Re}; //1.dizideki notalar
+int dizi2[]= {La, Sol, La, Fa, Sol, Fa, Mi, Re, Fa, Mi, Re, Re,Re}; //2.dizideki notalar
 int dizi3[]= {Si, Do, Do, Do, Do, Si, La, Do, Si, Si, Si, La, Sol, Si, La, La, La, Sol, Fa, La, Sol,Sol};  //3.dizideki notalar
 int dizi4[]= {Sol, Fa, Mi, Sol, Fa, Fa, Fa, Fa, Mi, Re, Fa, Mi, Mi, Mi, Re, Do, Mi, Re, Re, Re};  //4.dizideki notalar
             
@@ -24,7 +24,7 @@ Serial.begin(9600);  //Seri veri girişi başlatılır.
 
 void loop()
 {
-for (int i = 1; i <= notasayisi1; i++) //1. dizide bulunan 10 notanın sırasıyla çalması için döngü kullanılır. 
+for (int i = 0; i < notasayisi1; i++) //1. dizide bulunan 10 notanın sırasıyla çalması için döngü kullanılır. 
 {
 tone(buzzerpin, dizi1[i]); // 1. dizide bulunan notalar sırasıyla çalmaya başlar 
 Serial.println(dizi1[i]);  //Çalan notaların frekans değerleri ekrana yazdırılır. 
@@ -33,7 +33,7 @@ delay(310); //Her nota arasında 310 msn bekleme süresi vardır.
 noTone(buzzerpin); //Baştan sonra notalar çalması durdurulur.
 delay(750); //1sn beklenir.
 
-for (int i = 1; i <= notasayisi2; i++)//2. dizide bulunan 12 notanın sırasıyla çalması için döngü kullanılır. 
+for (int i = 0; i <= notasayisi2; i++)//2. dizide bulunan 12 notanın sırasıyla çalması için döngü kullanılır. 
 {
 tone(buzzerpin, dizi2[i]); //2. dizide bulunan notalar sırasıyla çalmaya başlar 
 Serial.println(dizi2[i]); //Çalan notaların frekans değerleri ekrana yazdırılır. 
@@ -42,7 +42,7 @@ delay(300); //Her nota arasında 300 msn bekleme süresi vardır.
 noTone(buzzerpin); //Sondan başa notaların çalması durdurulur. 
 delay(1000);// 1 sn beklenir. 
 
-for (int i = 1; i <= notasayisi3; i++)//3. dizide bulunan 22 notanın sırasıyla çalması için döngü kullanılır. 
+for (int i = 0; i <= notasayisi3; i++)//3. dizide bulunan 22 notanın sırasıyla çalması için döngü kullanılır. 
 {
 tone(buzzerpin, dizi3[i]); //3. dizide bulunan notalar çalmaya başlar.
 Serial.println(dizi3[i]); //Çalan notaların frekans değerleri ekrana yazdırılır. 
@@ -50,7 +50,7 @@ delay(300); //Her nota arasında300 msn bekleme süresi vardır.
 }      
 noTone(buzzerpin); //Notaların çalması durdurulur. 
 
-for (int i = 1; i <= notasayisi4; i++)//4. dizide bulunan 20 notanın sırasıyla çalması için döngü kullanılır. 
+for (int i = 0; i <= notasayisi4; i++)//4. dizide bulunan 20 notanın sırasıyla çalması için döngü kullanılır. 
 {
 tone(buzzerpin, dizi4[i]); //4. dizide bulunan notalar çalmaya başlar. 
 Serial.println(dizi4[i]); //Çalan notaların frekans değerleri ekrana yazdırılır. 
@@ -60,5 +60,3 @@ noTone(buzzerpin); //Notaların çalması durdurulur.
 delay(1000);// 1 sn beklenir. 
 
 }
-
-
