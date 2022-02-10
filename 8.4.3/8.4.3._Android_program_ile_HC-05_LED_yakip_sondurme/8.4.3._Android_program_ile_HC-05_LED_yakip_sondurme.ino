63f9cb651ab7a1e5cb8711c85e7daa69dc889253
Mehmet Akif Ocak <35992580+maocak@users.noreply.github.com>
2022-02-10 12:03:30 +0300

    Update 8.4.3._Android_program_ile_HC-05_LED_yakip_sondurme.ino

--- 8.4.3/8.4.3._Android_program_ile_HC-05_LED_yakip_sondurme/8.4.3._Android_program_ile_HC-05_LED_yakip_sondurme.ino
+++ 8.4.3/8.4.3._Android_program_ile_HC-05_LED_yakip_sondurme/8.4.3._Android_program_ile_HC-05_LED_yakip_sondurme.ino
@@ -1,20 +1,24 @@
-int ledpin = 13; //LED pini tanımlanır. 
-int durum=0;  //durum değişkenine ilk değer atanır. 
+#define ledpin 13 //LED pini tanımlanır. 
+int durum = 0; //durum değişkenine ilk değer atanır.
 
-void setup(){
-Serial.begin(38400);  //seri veri iletişimi başlatılır. 
-pinMode(ledpin,OUTPUT); //LED pini çıkış olarak tanımlanır. 
-}
-
-void loop(){
-if(Serial.available()!=0){ //Eğer seri port ekranından giriş yapılırsa...
-durum= Serial.read();  //girilen değer durum adlı değişkene atanır. 
-}
-if (durum =='h'){  //Girilen değer eğer h ise LED yanar. 
-digitalWrite(ledpin,HIGH);
-}
-if(durum=='l'){  //Girilen değer l ise LED söner. 
-digitalWrite(ledpin,LOW);
-}
+void setup() {
+pinMode(ledpin, OUTPUT); //LED pini çıkış olarak tanımlanır.
+digitalWrite(ledpin, LOW); //LEDin ilk durumu LOW(sönük) olarak ayarlanır. 
+Serial.begin(38400); //seri veri iletişimi başlatılır.
 }
 
+void loop() {
+if(Serial.available() > 0){ //Eğer seri port ekranından giriş yapılırsa...
+durum = Serial.read(); //girilen değer durum adlı değişkene atanır.
+}
+
+if (durum == '0') {
+digitalWrite(ledpin, LOW); // //Girilen değer 0 ise LED söner.
+durum = 0;
+}
+else if (durum == '1') {
+digitalWrite(ledpin, HIGH); //Girilen değer eğer h ise LED yanar.
+durum = 0;
+}
+delay (50);
+} 
